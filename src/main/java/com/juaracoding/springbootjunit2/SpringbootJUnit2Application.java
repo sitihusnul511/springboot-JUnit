a9f60e58055d7a1fb15e9988534eb32fbc6b4b0a
sitihusnul511 <sitihusnul511@gmail.com>
2022-04-09 20:49:38 +0700

    Upload project Springboot JUnit

---
+++ src/main/java/com/juaracoding/springbootjunit2/SpringbootJUnit2Application.java
@@ -0,0 +1,13 @@
+package com.juaracoding.springbootjunit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJUnit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJUnit2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/springbootjunit2/SpringbootJUnit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.springbootjunit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJUnit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
